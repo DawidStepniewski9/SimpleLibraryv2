71e4128a36e5f8ee228b40b1fbc4f65dc11aad8b
DawidStepniewski9 <dawlew999@gmail.com>
2024-03-11 17:49:47 +0100

    Add project files.

---
+++ SimpleLibraryv2/DAL/DbContextSimpleLibrary.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleLibraryv2.Models;
+
+namespace SimpleLibraryv2.DAL
+{
+    public class DbContextSimpleLibrary : DbContext
+    {
+        public DbContextSimpleLibrary() : base()
+               {
+
+               }
+
+        public DbSet<Book> book {  get; set; }
+    }
+}
---
+++ SimpleLibraryv2/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleLibraryv2.Models
+{
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
